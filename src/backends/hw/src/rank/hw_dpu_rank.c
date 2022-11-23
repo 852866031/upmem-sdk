d424b2e8dfd97770c5ca665cfccb5f088da6d08b
852866031 <chenlibert@gmail.com>
2022-11-22 20:27:55 -0500

    add prints

--- src/backends/hw/src/rank/hw_dpu_rank.c
+++ src/backends/hw/src/rank/hw_dpu_rank.c
@@ -632,6 +632,7 @@ hw_commit_commands(struct dpu_rank_t *rank, dpu_rank_buffer_t buffer)
             }
             /* fall through */
         case DPU_REGION_MODE_SAFE:
+            system("echo in handler");
 	        for(i=0; i<DPU_MAX_NR_CIS; i++){
                 printf("%lu\n", ptr_buffer[i]);
 	        }
