99688d26f87ff0dfb76eb0e6271c0dc80c67b8b6
852866031 <chenlibert@gmail.com>
2022-11-22 21:07:26 -0500

    add prints

--- src/backends/hw/src/rank/hw_dpu_rank.c
+++ src/backends/hw/src/rank/hw_dpu_rank.c
@@ -651,7 +651,7 @@ hw_update_commands(struct dpu_rank_t *rank, dpu_rank_buffer_t buffer)
     hw_dpu_rank_allocation_parameters_t params = _this_params(rank->description);
     dpu_rank_buffer_t ptr_buffer = buffer;
     int ret;
-    LOGFN(WARNING, "here here");
+    LOG_FN(WARNING, "here here");
     switch (params->mode) {
         case DPU_REGION_MODE_PERF:
             params->translate.read_from_cis(&params->translate,
