0a15bdd085c5977a77bcc8e6b3f396fcaa7725fe
852866031 <chenlibert@gmail.com>
2022-12-07 16:26:08 -0500

    add print for mram

--- src/backends/ufi/src/ufi_memory.c
+++ src/backends/ufi/src/ufi_memory.c
@@ -428,9 +428,8 @@ static dpu_error_t do_mram_transfer(struct dpu_rank_t *rank,
 
 	uintptr_t host_ptr = 0;
 	unsigned each_dpu;
-	printf("lol\n");
 	LOG_RANK(DEBUG, rank, "enter");
-
+	LOG_RANK(VERBOSE, rank, "lol");
 	for (each_dpu = 0; each_dpu < MAX_NR_DPUS_PER_RANK; each_dpu++) {
 		host_ptr |= (uintptr_t)matrix->ptr[each_dpu];
 	}
