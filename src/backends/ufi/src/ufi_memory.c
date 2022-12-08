de983ce188062b6edf6af9727f07e94324d1f6d3
852866031 <chenlibert@gmail.com>
2022-12-08 15:31:25 -0500

    sdkAction

--- src/backends/ufi/src/ufi_memory.c
+++ src/backends/ufi/src/ufi_memory.c
@@ -429,13 +429,12 @@ static dpu_error_t do_mram_transfer(struct dpu_rank_t *rank,
 	uintptr_t host_ptr = 0;
 	unsigned each_dpu;
 	LOG_RANK(WARNING, rank, "enter");
-	LOG_FN(WARNING, "WTF");
 	for (each_dpu = 0; each_dpu < MAX_NR_DPUS_PER_RANK; each_dpu++) {
 		host_ptr |= (uintptr_t)matrix->ptr[each_dpu];
 	}
 	if (!host_ptr) {
 		LOG_RANK(
-			DEBUG, rank,
+			WARNING, rank,
 			"transfer matrix does not contain any host ptr, do nothing");
 		return status;
 	}
