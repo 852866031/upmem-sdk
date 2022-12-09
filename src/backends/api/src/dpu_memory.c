e3c2b51c92292991ff281f2d3693936b9c0976cc
852866031 <chenlibert@gmail.com>
2022-12-09 17:42:05 -0500

    more prints

--- src/backends/api/src/dpu_memory.c
+++ src/backends/api/src/dpu_memory.c
@@ -266,7 +266,7 @@ dpu_copy_from_mram(struct dpu_t *dpu, uint8_t *destination, mram_addr_t mram_byt
     transfer_matrix.size = nb_of_bytes;
     transfer_matrix.offset = mram_byte_offset;
     status = dpu_copy_from_mrams(rank, &transfer_matrix);
-    int k;
+    uint32_t k;
 	for (k = 0; k<transfer_matrix.size; k++) {
 		if(*((uint8_t *) transfer_matrix.ptr[0] + k) != 0){
             printf("%d, at index : %d", *((uint8_t *) transfer_matrix.ptr[0] + k),k);
