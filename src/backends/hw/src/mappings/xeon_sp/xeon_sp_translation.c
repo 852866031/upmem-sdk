ea8becee9341a2ce52030227b4528e88c1a37aa3
Dufy Teguia <dteguia396@gmail.com>
2023-02-01 17:46:10 +0100

    s

--- src/backends/hw/src/mappings/xeon_sp/xeon_sp_translation.c
+++ src/backends/hw/src/mappings/xeon_sp/xeon_sp_translation.c
@@ -459,20 +459,22 @@ threads_write_to_rank(struct xeon_sp_private *xeon_sp_priv, uint8_t dpu_id_start
         double total = 0;
         
         for (i = 0; i < size_transfer / sizeof(uint64_t); ++i) {
-            clock_t start = clock();
+            
             
             uint32_t mram_64_bit_word_offset = apply_address_translation_on_mram_offset(i * 8 + offset) / 8;
             uint64_t next_data = BANK_OFFSET_NEXT_DATA(mram_64_bit_word_offset * sizeof(uint64_t));
             uint64_t offset = (next_data % BANK_CHUNK_SIZE) + (next_data / BANK_CHUNK_SIZE) * BANK_NEXT_CHUNK_OFFSET;
             
-            clock_t end = clock();
-            double time_elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
-            total+= time_elapsed;
-
+            
+            clock_t start = clock();
+            
             for (ci_id = 0; ci_id < nb_cis; ++ci_id) {
                 if (xfer_matrix->ptr[idx + ci_id])
                     cache_line[ci_id] = *((uint64_t *)xfer_matrix->ptr[idx + ci_id] + i);
             }
+            clock_t end = clock();
+            double time_elapsed = ((double)(end - start)) / CLOCKS_PER_SEC;
+            total+= time_elapsed;
 
             byte_interleave_avx512(cache_line, (uint64_t *)((uint8_t *)ptr_dest + offset), true);
             //byte_interleave_avx2(cache_line, (uint64_t *)((uint8_t *)ptr_dest + offset));
