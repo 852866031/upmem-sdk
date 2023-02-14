f8305519e90bebb3cf7ad3bac03b7ba99999da6c
Dufy Teguia <dteguia396@gmail.com>
2023-02-14 14:27:58 +0100

    dd'

--- src/backends/hw/src/mappings/xeon_sp/xeon_sp_translation.c
+++ src/backends/hw/src/mappings/xeon_sp/xeon_sp_translation.c
@@ -447,16 +447,13 @@ double elapsed;
         uint32_t i;
         uint8_t *ptr_dest = (uint8_t *)xeon_sp_priv->base_region_addr + BANK_START(dpu_id);
         bool do_dpu_transfer = false;
-        clock_gettime(CLOCK_MONOTONIC, &start);
         for (ci_id = 0; ci_id < nb_cis; ++ci_id) {
             if (xfer_matrix->ptr[idx + ci_id]) {
                 do_dpu_transfer = true;
                 break;
             }
         }
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-         printf("Temps d'exécution write to rank : %.10f secondes\n", elapsed);
+        
 
         if (!do_dpu_transfer)
             continue;
@@ -467,10 +464,15 @@ double elapsed;
             
 
         for (i = 0; i < size_transfer / sizeof(uint64_t); ++i) {
+            clock_gettime(CLOCK_MONOTONIC, &start);
+
             uint32_t mram_64_bit_word_offset = apply_address_translation_on_mram_offset(i * 8 + offset) / 8;
             uint64_t next_data = BANK_OFFSET_NEXT_DATA(mram_64_bit_word_offset * sizeof(uint64_t));
             uint64_t offset = (next_data % BANK_CHUNK_SIZE) + (next_data / BANK_CHUNK_SIZE) * BANK_NEXT_CHUNK_OFFSET;
            
+           clock_gettime(CLOCK_MONOTONIC, &end);
+        elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
+         printf("Temps d'exécution offset : %.10f secondes\n", elapsed);
             
             for (ci_id = 0; ci_id < nb_cis; ++ci_id) {
                 if (xfer_matrix->ptr[idx + ci_id])
