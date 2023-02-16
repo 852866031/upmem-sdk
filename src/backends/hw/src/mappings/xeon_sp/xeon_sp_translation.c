958a0ba42a4b2949e9e526911b8c09de943a2059
Dufy Teguia <dteguia396@gmail.com>
2023-02-16 13:47:40 +0100

    dd'

--- src/backends/hw/src/mappings/xeon_sp/xeon_sp_translation.c
+++ src/backends/hw/src/mappings/xeon_sp/xeon_sp_translation.c
@@ -470,15 +470,15 @@ double elapsed;
             uint64_t next_data = BANK_OFFSET_NEXT_DATA(mram_64_bit_word_offset * sizeof(uint64_t));
             uint64_t offset = (next_data % BANK_CHUNK_SIZE) + (next_data / BANK_CHUNK_SIZE) * BANK_NEXT_CHUNK_OFFSET;
            
-clock_gettime(CLOCK_MONOTONIC, &start);
+
             for (ci_id = 0; ci_id < nb_cis; ++ci_id) {
                 if (xfer_matrix->ptr[idx + ci_id])
                     cache_line[ci_id] = *((uint64_t *)xfer_matrix->ptr[idx + ci_id] + i);
             }
-                     clock_gettime(CLOCK_MONOTONIC, &end);          
-
+                            
+clock_gettime(CLOCK_MONOTONIC, &start);
             byte_interleave_avx512(cache_line, (uint64_t *)((uint8_t *)ptr_dest + offset), true);
-           
+           clock_gettime(CLOCK_MONOTONIC, &end);   
             //byte_interleave_avx2(cache_line, (uint64_t *)((uint8_t *)ptr_dest + offset));
             /// /// /// /// /// /// ///
         }
@@ -499,7 +499,7 @@ clock_gettime(CLOCK_MONOTONIC, &start);
         //clock_gettime(CLOCK_MONOTONIC, &end);
 
            elapsed = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-         printf("Temps d'exécution off : %.10f secondes\n", elapsed);  
+         printf("Temps d'exécution av : %.10f secondes\n", elapsed);  
 }
 
 static void
